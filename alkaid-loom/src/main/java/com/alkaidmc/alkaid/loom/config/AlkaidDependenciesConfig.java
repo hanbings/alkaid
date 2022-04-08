e59a2b823dc079df388f34aa747caafe3405f1bc
hanbings <3219065882@qq.com>
2022-04-08 09:55:41 +0800

    🔥 Chore: Move alkaid-loom module to alkaid-loom repository.

--- alkaid-loom/src/main/java/com/alkaidmc/alkaid/loom/config/AlkaidDependenciesConfig.java
+++
@@ -1,9 +0,0 @@
-package com.alkaidmc.alkaid.loom.config;
-
-import org.gradle.api.provider.Property;
-
-import java.util.List;
-
-public abstract class AlkaidDependenciesConfig {
-    public abstract Property<List<String>> getDependencies();
-}
