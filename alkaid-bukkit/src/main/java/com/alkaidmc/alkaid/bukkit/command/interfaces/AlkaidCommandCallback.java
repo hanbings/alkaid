8ead99272aef4774e6bac7a89b15c818c356807a
hanbings <3219065882@qq.com>
2022-04-14 16:02:30 +0800

    ✨ Feat: Regex parser in command module.

--- alkaid-bukkit/src/main/java/com/alkaidmc/alkaid/bukkit/command/interfaces/AlkaidCommandCallback.java
+++ alkaid-bukkit/src/main/java/com/alkaidmc/alkaid/bukkit/command/interfaces/AlkaidCommandCallback.java
@@ -1,7 +1,8 @@
 package com.alkaidmc.alkaid.bukkit.command.interfaces;
 
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
 public interface AlkaidCommandCallback {
-    boolean execute(CommandSender sender, String[] args);
+    boolean execute(CommandSender sender, Command command, String aliases, String[] args);
 }
--- alkaid-bukkit/src/main/java/com/alkaidmc/alkaid/bukkit/command/interfaces/AlkaidTabCallback.java
+++ alkaid-bukkit/src/main/java/com/alkaidmc/alkaid/bukkit/command/interfaces/AlkaidTabCallback.java
@@ -1,9 +1,10 @@
 package com.alkaidmc.alkaid.bukkit.command.interfaces;
 
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
 import java.util.List;
 
 public interface AlkaidTabCallback {
-    List<String> tab(CommandSender sender, String[] args);
+    List<String> tab(CommandSender sender, Command command, String aliases, String[] args);
 }
