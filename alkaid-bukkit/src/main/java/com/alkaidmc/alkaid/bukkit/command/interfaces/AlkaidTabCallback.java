cc71f05af6780cd136a7b46516e755c3e3163e29
hanbings <3219065882@qq.com>
2022-04-14 10:06:38 +0800

    🔥 Chore: Remove command stream parser.

--- alkaid-bukkit/src/main/java/com/alkaidmc/alkaid/bukkit/command/interfaces/AlkaidTabCallback.java
+++
@@ -1,9 +0,0 @@
-package com.alkaidmc.alkaid.bukkit.command.interfaces;
-
-import org.bukkit.command.CommandSender;
-
-import java.util.List;
-
-public interface AlkaidTabCallback {
-    List<String> type(CommandSender sender, String[] args);
-}
