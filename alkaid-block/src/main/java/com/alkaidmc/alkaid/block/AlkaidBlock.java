76d1180f233808ca06fd91f11948cfd6157e08cb
hanbings <3219065882@qq.com>
2022-04-18 17:19:06 +0800

    👷 Chore: Refactor package structure.

---
+++ alkaid-block/src/main/java/com/alkaidmc/alkaid/block/AlkaidBlock.java
@@ -0,0 +1,11 @@
+package com.alkaidmc.alkaid.block;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AlkaidBlock {
+    JavaPlugin plugin;
+
+    public AlkaidBlock(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+}
