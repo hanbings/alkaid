0e36b976abe8cc922315ffa4ab0a78465996f85a
hanbings <3219065882@qq.com>
2022-04-21 23:35:03 +0800

    🔨 Refact: Refactor event package.

--- alkaid-bukkit/src/main/java/com/alkaidmc/alkaid/bukkit/event/ConditionalEventRegister.java
+++ alkaid-bukkit/src/main/java/com/alkaidmc/alkaid/bukkit/event/ConditionalEventRegister.java
@@ -1,7 +1,10 @@
 package com.alkaidmc.alkaid.bukkit.event;
 
-import com.alkaidmc.alkaid.bukkit.event.interfaces.AlkaidEventConditional;
 import com.alkaidmc.alkaid.bukkit.event.interfaces.AlkaidEventRegister;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -10,79 +13,64 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.function.Consumer;
 
 @SuppressWarnings("unused")
-public class ConditionalEventRegister implements AlkaidEventRegister, AlkaidEventConditional {
-    JavaPlugin plugin;
+@RequiredArgsConstructor
+public class ConditionalEventRegister implements AlkaidEventRegister {
+    final JavaPlugin plugin;
     // 需要监听的事件
+    @Setter
+    @Getter
+    @Accessors(fluent = true, chain = true)
     Class<? extends Event> event;
     // 结束条件的事件
+    @Setter
+    @Getter
+    @Accessors(fluent = true, chain = true)
     Class<? extends Event> interrupt;
     // 事件处理器
-    Consumer<Event> consumer;
+    @Setter
+    @Getter
+    @Accessors(fluent = true, chain = true)
+    Consumer<Event> use;
     // Bukkit 事件优先级
+    @Setter
+    @Getter
+    @Accessors(fluent = true, chain = true)
     EventPriority priority = EventPriority.NORMAL;
     // 是否忽略  Bukkit 事件的取消标志
+    @Setter
+    @Getter
+    @Accessors(fluent = true, chain = true)
     boolean ignore = false;
+
     // 注销事件
     boolean cancel = false;
-    // 监听器
-    Listener listener = new Listener() {
-        public void on(Event event) {
-            // 判断该事件是否注销
-            if (cancel) {
-                event.getHandlers().unregister(this);
-                return;
-            }
-            consumer.accept(event);
-        }
-    };
-
-    public ConditionalEventRegister(JavaPlugin plugin) {
-        this.plugin = plugin;
-    }
 
     @Override
-    public ConditionalEventRegister listener(Class<? extends Event> event) {
-        this.event = event;
-        return this;
-    }
-
-    @Override
-    public ConditionalEventRegister use(Consumer<Event> consumer) {
-        this.consumer = consumer;
-        return this;
-    }
-
-    @Override
-    public ConditionalEventRegister priority(EventPriority priority) {
-        this.priority = priority;
-        return this;
-    }
-
-    @Override
-    public ConditionalEventRegister ignore(boolean ignore) {
-        this.ignore = ignore;
-        return this;
-    }
-
-    @Override
-    public ConditionalEventRegister register() {
+    public void register() {
         plugin.getServer().getPluginManager().registerEvent(
-                event, listener, priority, (l, e) -> consumer.accept(e), plugin, ignore
+                event,
+                new Listener() {
+                },
+                priority,
+                (l, e) -> {
+                    // 判断该事件是否注销
+                    if (cancel) {
+                        e.getHandlers().unregister(l);
+                        return;
+                    }
+                    use.accept(e);
+                },
+                plugin,
+                ignore
         );
         plugin.getServer().getPluginManager().registerEvent(
-                interrupt, listener, priority, (l, e) -> cancel = true, plugin, ignore
+                interrupt, new Listener() {
+                }, priority, (l, e) -> cancel = true, plugin, ignore
         );
-        return this;
     }
 
     @Override
     public void unregister() {
         cancel = true;
-    }
-
-    @Override
-    public ConditionalEventRegister interrupt(Class<? extends Event> event) {
-        this.interrupt = event;
-        return this;
     }
 }
--- alkaid-bukkit/src/main/java/com/alkaidmc/alkaid/bukkit/event/interfaces/AlkaidEventRegister.java
+++ alkaid-bukkit/src/main/java/com/alkaidmc/alkaid/bukkit/event/interfaces/AlkaidEventRegister.java
@@ -1,21 +1,8 @@
 package com.alkaidmc.alkaid.bukkit.event.interfaces;
 
-import org.bukkit.event.Event;
-import org.bukkit.event.EventPriority;
-
-import java.util.function.Consumer;
-
 @SuppressWarnings("unused")
 public interface AlkaidEventRegister {
-    AlkaidEventRegister listener(Class<? extends Event> event);
-
-    AlkaidEventRegister use(Consumer<Event> consumer);
-
-    AlkaidEventRegister priority(EventPriority priority);
-
-    AlkaidEventRegister ignore(boolean ignore);
-
-    AlkaidEventRegister register();
+    void register();
 
     void unregister();
 }
