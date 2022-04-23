a8c257a44aebdef6bfc74106422339441e4501bb
hanbings <3219065882@qq.com>
2022-04-23 09:10:34 +0800

    📝 Docs: Update readme.

---
+++ alkaid-bukkit/src/main/java/com/alkaidmc/alkaid/bukkit/message/ChatMessage.java
@@ -0,0 +1,31 @@
+package com.alkaidmc.alkaid.bukkit.message;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatMessage {
+    List<BaseComponent> components = new ArrayList<>();
+
+    public ChatMessage text(String text) {
+        components.add(new TextComponent(text));
+        return this;
+    }
+
+    public ChatMessage text(String text, ChatColor color) {
+        TextComponent component = new TextComponent(text);
+        component.setColor(color);
+        components.add(component);
+        return this;
+    }
+
+    // todo 悬空字、文字属性等实现
+
+    public ChatMessage newline() {
+        components.add(new TextComponent("\n"));
+        return this;
+    }
+}
