55a8c8ddf5117daaeb488a428f237489c8ec6361
hanbings <3219065882@qq.com>
2022-04-28 17:03:38 +0800

    🚧 Feat: Try design gui inventory.

--- alkaid-inventory/src/main/java/com/alkaidmc/alkaid/inventory/AlkaidInventory.java
+++ alkaid-inventory/src/main/java/com/alkaidmc/alkaid/inventory/AlkaidInventory.java
@@ -1,10 +1,13 @@
 package com.alkaidmc.alkaid.inventory;
 
+import org.bukkit.inventory.InventoryHolder;
+
 public class AlkaidInventory {
     public BookInventory book() {
         return new BookInventory();
     }
-    public GuiInventory gui() {
-        return new GuiInventory();
+
+    public GuiInventory gui(int rows, InventoryHolder holder) {
+        return new GuiInventory(rows, holder);
     }
 }
