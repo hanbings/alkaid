7c41c06abe8900a6cb0aa09a9dcb5d559a1826e4
hanbings <3219065882@qq.com>
2022-04-29 19:19:06 +0800

    🚧 Refact: Refactor todo list.

--- alkaid-common/src/main/java/com/alkaidmc/alkaid/common/lang/ReflectionActions.java
+++ alkaid-common/src/main/java/com/alkaidmc/alkaid/common/lang/ReflectionActions.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 import java.util.function.Consumer;
 
 public class ReflectionActions {
+    // todo： classloader 以及错误处理参数
     @Getter
     @Accessors(fluent = true, chain = true)
     Class<?> clazz;
