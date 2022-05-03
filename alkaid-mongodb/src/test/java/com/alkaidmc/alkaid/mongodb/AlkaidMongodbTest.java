97fa1cecd2956bab5f9a5ab3019d48c6dd517055
NekokeCore <Core2002@163.com>
2022-05-03 22:12:36 +0800

    ✨ Feat: Add test method for insertion.

--- alkaid-mongodb/src/test/java/com/alkaidmc/alkaid/mongodb/AlkaidMongodbTest.java
+++ alkaid-mongodb/src/test/java/com/alkaidmc/alkaid/mongodb/AlkaidMongodbTest.java
@@ -1,10 +1,17 @@
 package com.alkaidmc.alkaid.mongodb;
 
+import org.bson.Document;
 import org.junit.jupiter.api.Test;
 
 public class AlkaidMongodbTest {
-    @Test
-    public void test() {
+    AsyncMongodbConnection asyncMongodbConnection = new AlkaidMongodb().database("test").async();
+    String testCollection = "testAlkaid";
 
+    @Test
+    public void insertionTest() {
+        asyncMongodbConnection.create(testCollection,
+                new Document("name", "Neko")
+                        .append("age", 4)
+        );
     }
 }
