42a2de99b0f47409758a0691fe2b09253d78669d
hanbings <3219065882@qq.com>
2022-05-08 20:59:17 +0800

    ✨ Feat: Redis single server mode was support. (#2)

--- alkaid-redis/src/main/java/com/alkaidmc/alkaid/redis/AlkaidRedis.java
+++ alkaid-redis/src/main/java/com/alkaidmc/alkaid/redis/AlkaidRedis.java
@@ -16,60 +16,9 @@
 
 package com.alkaidmc.alkaid.redis;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.experimental.Accessors;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-
-import java.util.Optional;
-
-@Setter
-@Getter
 @SuppressWarnings("unused")
-@Accessors(fluent = true, chain = true)
 public class AlkaidRedis {
-    String host = "localhost";
-    int port = 6379;
-    String user;
-    String password;
-
-    private JedisPool jedisPool = new JedisPool();
-    int maxTotal = 100;
-    int maxIdle = 10;
-    int minIdle = 0;
-    int maxWait = -1;
-    boolean testOnBorrow = false;
-    boolean testOnReturn = false;
-    boolean testWhileIdle = false;
-    int timeBetweenEvictionRunsMillis = -1;
-    int numThreads = -1;
-    int minEvictableIdleTimeMillis = -1;
-    boolean softMinEvictableIdleTimeMillis = false;
-    boolean lifo = false;
-    boolean fairness = false;
-    boolean jmxEnabled = false;
-
-
-    // redis 链式配置文件
-    // https://github.com/AlkaidMC/alkaid/projects/1#card-81556630
-    public RedisConnector redis() {
-        jedisPool.setMaxTotal(maxTotal);
-        jedisPool.setMaxIdle(maxIdle);
-        jedisPool.setMinIdle(minIdle);
-        jedisPool.setMaxWaitMillis(maxWait);
-        jedisPool.setTestOnBorrow(testOnBorrow);
-        jedisPool.setTestOnReturn(testOnReturn);
-        jedisPool.setTestWhileIdle(testWhileIdle);
-        jedisPool.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
-        jedisPool.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
-        jedisPool.setLifo(lifo);
-        Jedis resource = jedisPool.getResource();
-
-        if (user != null && password != null)
-            resource.auth(user, password);
-        else if (user == null && password != null)
-            resource.auth(password);
-        return new RedisConnector(resource);
+    public SingleRedisConnector single() {
+        return new SingleRedisConnector();
     }
 }
---
+++ alkaid-redis/src/main/java/com/alkaidmc/alkaid/redis/SingleRedisConnector.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2022 Alkaid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *           http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alkaidmc.alkaid.redis;
+
+import com.google.gson.Gson;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.Optional;
+
+@Setter
+@Getter
+@SuppressWarnings("unused")
+@Accessors(fluent = true, chain = true)
+public class SingleRedisConnector {
+    String host = "127.0.0.1";
+    int port = 6379;
+    String auth = null;
+    int connect = 8;
+    int timeout = 1000;
+    long sleep = 1000;
+    Gson gson;
+    JedisPoolConfig config = new JedisPoolConfig();
+
+    @Setter(AccessLevel.NONE)
+    @Getter(AccessLevel.NONE)
+    JedisPool pool;
+
+    public SingleRedisConnector connect() {
+        pool = Optional.ofNullable(auth)
+                .map(password -> new JedisPool(config, host, port, timeout, password))
+                .orElseGet(() -> new JedisPool(config, host, port, timeout));
+
+        return this;
+    }
+
+    public void disconnect() {
+        pool.destroy();
+    }
+
+    public SingleRedisConnection connection() {
+        return new SingleRedisConnection(pool, Optional.ofNullable(gson).orElse(new Gson()), sleep);
+    }
+}
