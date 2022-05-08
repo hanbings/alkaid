024c28fbf04ee28e92c45c0d35a822a3411e6677
hanbings <3219065882@qq.com>
2022-05-08 20:57:32 +0800

    ✨ Feat: Redis single server mode was support.

---
+++ alkaid-redis/src/main/java/com/alkaidmc/alkaid/redis/SingleRedisConnection.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2022 Alkaid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *           http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alkaidmc.alkaid.redis;
+
+import com.google.gson.Gson;
+import lombok.RequiredArgsConstructor;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPubSub;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+
+@RequiredArgsConstructor
+@SuppressWarnings("unused")
+public class SingleRedisConnection {
+    final static ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+    final JedisPool pool;
+    final Gson gson;
+    final long sleep;
+    // 监听控制器
+    boolean listening = false;
+
+    public <T> void set(String key, T value) {
+        pool.getResource().set(key, gson.toJson(value));
+        pool.close();
+    }
+
+    public <T> T get(String key, Class<T> clazz) {
+        String value = pool.getResource().get(key);
+        pool.close();
+        return gson.fromJson(value, clazz);
+    }
+
+    public void del(String key) {
+        pool.getResource().del(key);
+        pool.close();
+    }
+
+    public void expire(String key, int seconds) {
+        pool.getResource().expire(key, seconds);
+        pool.close();
+    }
+
+    public boolean exists(String key) {
+        boolean exists = pool.getResource().exists(key);
+        pool.close();
+        return exists;
+    }
+
+    public void publish(String channel, String message) {
+        pool.getResource().publish(channel, message);
+        pool.close();
+    }
+
+    public void subscribe(String channel, Consumer<String> message) {
+        scheduler.schedule(() -> {
+            if (listening) {
+                pool.getResource().subscribe(new JedisPubSub() {
+                    @Override
+                    public void onMessage(String ch, String msg) {
+                        message.accept(msg);
+                    }
+                }, channel);
+            }
+        }, sleep, TimeUnit.MILLISECONDS);
+    }
+
+    public void unsubscribe() {
+        this.listening = false;
+    }
+}
