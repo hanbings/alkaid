4a7f98c9b17b47618c8220b48c6d575e1aed5912
hanbings <3219065882@qq.com>
2022-05-11 19:36:52 +0800

    ✨ Feat: Add chain event register.

--- alkaid-bukkit/src/main/java/com/alkaidmc/alkaid/bukkit/event/ChainEventRegister.java
+++ alkaid-bukkit/src/main/java/com/alkaidmc/alkaid/bukkit/event/ChainEventRegister.java
@@ -1,27 +1,52 @@
 package com.alkaidmc.alkaid.bukkit.event;
 
 import com.alkaidmc.alkaid.bukkit.event.interfaces.AlkaidEventRegister;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.plugin.EventExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.function.Consumer;
+
 
 @RequiredArgsConstructor
 @SuppressWarnings("unused")
 public class ChainEventRegister implements AlkaidEventRegister {
     final JavaPlugin plugin;
 
-    final List<SimpleEventRegister<? extends Event>> chain;
-    final Map<Class<? extends Event>, Integer> index;
-    final Map<UUID, Integer> schedules;
-    final int schedule;
-    final boolean multi;
+    // 事件处理器 / Event handler.
+    @Setter
+    @Getter
+    @Accessors(fluent = true, chain = true)
+    Consumer<? extends Event> listener = event -> {
+    };
+    // Bukkit 事件优先级 / Bukkit event priority.
+    @Setter
+    @Getter
+    @Accessors(fluent = true, chain = true)
+    EventPriority priority = EventPriority.NORMAL;
+    // 是否忽略  Bukkit 事件的取消标志 / Whether to ignore Bukkit event cancellation flag.
+    @Setter
+    @Getter
+    @Accessors(fluent = true, chain = true)
+    boolean ignore = false;
+
+    List<SimpleEventRegister<? extends Event>> chain = new ArrayList<>();
+    Map<Class<? extends Event>, Integer> index = new HashMap<>();
+    Map<UUID, Integer> schedules;
+    int schedule;
+    boolean multi = false;
 
     public <T extends Event> ChainEventRegister skewer(Class<T> event) {
         return skewer(event, t -> {
@@ -37,21 +62,79 @@ public class ChainEventRegister implements AlkaidEventRegister {
                                                        EventPriority priority, boolean ignore,
                                                        int index) {
         chain.add(index, new SimpleEventRegister<>(plugin, event) {
+                    final ChainEventRegister chain = ChainEventRegister.this;
+
                     @Override
                     public void register() {
+                        EventExecutor executor;
+
+                        if (multi) {
+                            if (schedules == null) {
+                                schedules = new HashMap<>();
+                            }
+
+                            executor = (l, e) -> {
+                                // 判断该事件是否注销 / Check if the event is cancelled.
+                                // 判断事件是否在索引中 / Check if the event is in the index.
+                                if (cancel ||
+                                        !chain.index.containsKey(e.getClass()) ||
+                                        !(e instanceof PlayerEvent)
+                                ) {
+                                    e.getHandlers().unregister(l);
+                                    return;
+                                }
+
+                                // 获取索引 / Get the index.
+                                int index = chain.index.get(e.getClass());
+                                // 获取进度 / Get the progress.
+                                int progress = schedules.getOrDefault(
+                                        ((PlayerEvent) e).getPlayer().getUniqueId(),
+                                        0
+                                );
+
+                                // 判断进度是否达到预期 / Check if the progress is reached.
+                                if (progress != 0 && progress != index - 1) {
+                                    return;
+                                }
+
+                                // 设置进度 / Set the progress.
+                                schedules.put(
+                                        ((PlayerEvent) e).getPlayer().getUniqueId(),
+                                        progress
+                                );
+
+                                listener.accept((T) e);
+                            };
+                        } else {
+                            executor = (l, e) -> {
+                                if (cancel ||
+                                        !chain.index.containsKey(e.getClass())
+                                ) {
+                                    e.getHandlers().unregister(l);
+                                    return;
+                                }
+
+                                // 获取索引 / Get the index.
+                                int index = chain.index.get(e.getClass());
+
+                                // 判断进度是否达到预期 / Check if the progress is reached.
+                                if (schedule != 0 && schedule != index - 1) {
+                                    return;
+                                }
+
+                                // 设置进度 / Set the progress.
+                                schedule = index;
+
+                                listener.accept((T) e);
+                            };
+                        }
+
                         plugin.getServer().getPluginManager().registerEvent(
                                 event,
                                 new Listener() {
                                 },
                                 priority,
-                                (l, e) -> {
-                                    // 判断该事件是否注销 / Check if the event is cancelled.
-                                    if (cancel) {
-                                        e.getHandlers().unregister(l);
-                                        return;
-                                    }
-                                    listener.accept((T) e);
-                                },
+                                executor,
                                 plugin,
                                 ignore
                         );
