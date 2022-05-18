45e3d194a3db38d4b5e1ca794f00ee128f5fc954
hanbings <3219065882@qq.com>
2022-05-18 12:37:29 +0800

    ✨ Feat: Add Lazy<T> function interface.

---
+++ alkaid-common/src/main/java/com/alkaidmc/alkaid/common/function/Lazy.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2022 Alkaid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *           http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alkaidmc.alkaid.common.function;
+
+import java.util.function.Supplier;
+
+@SuppressWarnings("unused")
+public class Lazy<T> {
+    private final Supplier<T> supplier;
+    private volatile T value;
+
+    public static <T> Lazy<T> of(Supplier<T> supplier) {
+        return new Lazy<>(supplier);
+    }
+
+    public Lazy(Supplier<T> supplier) {
+        this.supplier = supplier;
+    }
+
+    public T get() {
+        if (value == null) {
+            synchronized (this) {
+                if (value == null) {
+                    value = supplier.get();
+                }
+            }
+        }
+        return value;
+    }
+}
