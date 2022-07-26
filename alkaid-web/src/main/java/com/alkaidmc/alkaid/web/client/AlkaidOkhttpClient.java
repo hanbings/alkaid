cd107be9551e952fead6555c204036f397e12de4
hanbings <3219065882@qq.com>
2022-07-27 00:08:48 +0800

    ✨ Feat: Try to design the response to HTTP requests.

--- alkaid-web/src/main/java/com/alkaidmc/alkaid/web/client/AlkaidOkhttpClient.java
+++ alkaid-web/src/main/java/com/alkaidmc/alkaid/web/client/AlkaidOkhttpClient.java
@@ -1,4 +1,15 @@
 package com.alkaidmc.alkaid.web.client;
 
+import okhttp3.OkHttpClient;
+
 public class AlkaidOkhttpClient {
+    static OkHttpClient client;
+
+    static {
+        client = new OkHttpClient();
+    }
+
+    public void get(String url, OkhttpResponse response) {
+
+    }
 }
