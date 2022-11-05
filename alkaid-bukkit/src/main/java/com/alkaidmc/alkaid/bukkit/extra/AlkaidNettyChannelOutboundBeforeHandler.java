43c0b5de286f888a0aa58a053cafff06a9c3df4d
hanbings <3219065882@qq.com>
2022-11-05 22:41:14 +0800

    ✨ Feat: Added netty channel handler adapter.

--- alkaid-bukkit/src/main/java/com/alkaidmc/alkaid/bukkit/extra/AlkaidNettyChannelOutboundBeforeHandler.java
+++ alkaid-bukkit/src/main/java/com/alkaidmc/alkaid/bukkit/extra/AlkaidNettyChannelOutboundBeforeHandler.java
@@ -14,25 +14,9 @@
  * limitations under the License.
  */
 
-package com.alkaidmc.alkaid.bukkit.extra.event;
+package com.alkaidmc.alkaid.bukkit.extra;
 
-import lombok.RequiredArgsConstructor;
-import org.bukkit.event.Event;
-import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.NotNull;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
 
-@RequiredArgsConstructor
-@SuppressWarnings("unused")
-public class SecondEvent extends Event {
-    static final HandlerList HANDLERS_LIST = new HandlerList();
-    final long second;
-
-    @Override
-    public @NotNull HandlerList getHandlers() {
-        return HANDLERS_LIST;
-    }
-
-    public long getSecond() {
-        return this.second;
-    }
+public class AlkaidNettyChannelOutboundBeforeHandler extends ChannelOutboundHandlerAdapter {
 }
--- alkaid-bukkit/src/main/java/com/alkaidmc/alkaid/bukkit/extra/event/PacketReceivedEvent.java
+++ alkaid-bukkit/src/main/java/com/alkaidmc/alkaid/bukkit/extra/event/PacketReceivedEvent.java
@@ -16,23 +16,15 @@
 
 package com.alkaidmc.alkaid.bukkit.extra.event;
 
-import lombok.RequiredArgsConstructor;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
-@RequiredArgsConstructor
-@SuppressWarnings("unused")
-public class SecondEvent extends Event {
+public class PacketReceivedEvent extends Event {
     static final HandlerList HANDLERS_LIST = new HandlerList();
-    final long second;
 
     @Override
     public @NotNull HandlerList getHandlers() {
         return HANDLERS_LIST;
-    }
-
-    public long getSecond() {
-        return this.second;
     }
 }
