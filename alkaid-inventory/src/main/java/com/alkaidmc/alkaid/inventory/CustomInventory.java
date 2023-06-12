94f72e7183f76545c94643cf804573ecdd4d5fbf
hanbings <hanbings@hanbings.io>
2023-06-12 18:17:01 +0800

    ✨ Feat: Finished customizing the Inventory base skeleton.

--- alkaid-inventory/src/main/java/com/alkaidmc/alkaid/inventory/CustomInventory.java
+++ alkaid-inventory/src/main/java/com/alkaidmc/alkaid/inventory/CustomInventory.java
@@ -18,6 +18,8 @@ package com.alkaidmc.alkaid.inventory;
 
 import lombok.*;
 import lombok.experimental.Accessors;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
@@ -27,10 +29,13 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import java.util.function.Consumer;
-import java.util.function.Function;
 
 @Setter
 @Getter
@@ -40,20 +45,20 @@ import java.util.function.Function;
 public class CustomInventory {
     static final Listener LISTENER = new Listener() {
     };
-    static final Map<UUID, CustomInventory> INVENTORY_REGISTRY = new HashMap<>();
 
     // constructor
     final Plugin plugin;
 
     // builder
-    int rows;
-    long interval;
-    InventoryHolder holder;
-    Consumer<InventoryOpenEvent> open;
-    Consumer<InventoryCloseEvent> close;
-    Consumer<InventoryClickEvent> click;
-    Consumer<InventoryDragEvent> drag;
-    Function<List<ItemStackAction>, List<ItemStackAction>> update;
+    String title = "Alkaid Custom Inventory";
+    int rows = 9;
+    long interval = 20;
+    @Nullable Holder holder;
+    @Nullable Consumer<InventoryOpenEvent> open;
+    @Nullable Consumer<InventoryCloseEvent> close;
+    @Nullable Consumer<InventoryClickEvent> click;
+    @Nullable Consumer<InventoryDragEvent> drag;
+    @Nullable Update update;
 
     // instance
     @Getter(AccessLevel.NONE)
@@ -61,28 +66,102 @@ public class CustomInventory {
     Inventory inventory;
     @Getter(AccessLevel.NONE)
     @Setter(AccessLevel.NONE)
-    List<ItemStack> items = new ArrayList<>();
+    List<ItemStackRegistry> registries = new ArrayList<>();
     @Setter(AccessLevel.NONE)
     UUID uuid = UUID.randomUUID();
 
     public CustomInventory add(ItemStack item) {
-        items.add(item);
+        registries.add(new ItemStackRegistry(registries.size(), item, new ItemStackAction()));
         return this;
     }
 
     public CustomInventory add(ItemStackAction action, ItemStack item) {
+        registries.add(new ItemStackRegistry(registries.size(), item, action));
         return this;
     }
 
     public CustomInventory add(ItemStackAction action, ItemStack item, int... slot) {
+        for (int i : slot) registries.add(new ItemStackRegistry(i, item, action));
         return this;
     }
 
     public CustomInventory structure(char[][] shape, Map<Character, ItemStackRegistry> map) {
+        for (int row = 0; row < shape.length; row++) {
+            for (int col = 0; col < shape[row].length; col++) {
+                ItemStackRegistry registry = map.get(shape[row][col]);
+                if (registry == null) continue;
+                registry.slot(row * 9 + col);
+                registries.add(registry);
+            }
+        }
+
         return this;
     }
 
     public Inventory inventory() {
+        // create inventory
+        this.holder = new Holder(this);
+        this.inventory = Bukkit.createInventory(this.holder, this.rows * 9, this.title);
+
+        // register listener
+
+        // open
+        plugin.getServer().getPluginManager().registerEvent(InventoryOpenEvent.class, LISTENER, EventPriority.NORMAL, (l, e) -> {
+            if (!(((InventoryOpenEvent) e).getInventory().getHolder() instanceof Holder)) return;
+            Holder holder = (Holder) inventory.getHolder();
+            if (holder == null || holder.custom.uuid != uuid) return;
+            if (open != null) open.accept((InventoryOpenEvent) e);
+        }, plugin, false);
+
+        // close
+        plugin.getServer().getPluginManager().registerEvent(InventoryCloseEvent.class, LISTENER, EventPriority.NORMAL, (l, e) -> {
+            if (!(((InventoryCloseEvent) e).getInventory().getHolder() instanceof Holder)) return;
+            Holder holder = (Holder) inventory.getHolder();
+            if (holder == null || holder.custom.uuid != uuid) return;
+            if (close != null) close.accept((InventoryCloseEvent) e);
+        }, plugin, false);
+
+        // click
+        plugin.getServer().getPluginManager().registerEvent(InventoryClickEvent.class, LISTENER, EventPriority.NORMAL, (l, e) -> {
+            if (!(((InventoryClickEvent) e).getInventory().getHolder() instanceof Holder)) return;
+            Holder holder = (Holder) inventory.getHolder();
+            if (holder == null || holder.custom.uuid != uuid) return;
+            if (click != null) click.accept((InventoryClickEvent) e);
+
+            // item click callback
+            int slot = ((InventoryClickEvent) e).getSlot();
+            registries.forEach(r -> {
+                if (!(slot == r.slot)) return;
+
+                // click
+                if (r.action.click() != null) r.action().click().accept(inventory.getItem(slot));
+                // left
+                if (r.action().left() != null && ((InventoryClickEvent) e).isLeftClick())
+                    r.action().left().accept(inventory.getItem(slot));
+                // right
+                if (r.action.right != null && ((InventoryClickEvent) e).isRightClick())
+                    r.action().right().accept(inventory.getItem(slot));
+            });
+
+        }, plugin, false);
+
+        // drag
+        plugin.getServer().getPluginManager().registerEvent(InventoryDragEvent.class, LISTENER, EventPriority.NORMAL, (l, e) -> {
+            if (!(((InventoryDragEvent) e).getInventory().getHolder() instanceof Holder)) return;
+            Holder holder = (Holder) inventory.getHolder();
+            if (holder == null || holder.custom.uuid != uuid) return;
+            if (drag != null) drag.accept((InventoryDragEvent) e);
+
+            // item drag callback
+            int slot = ((InventoryDragEvent) e).getRawSlots().stream().findFirst().orElse(-1);
+            registries.forEach(r -> {
+                if (!(slot == r.slot)) return;
+
+                // drag
+                if (r.action().drag() != null) r.action().drag().accept(inventory.getItem(slot));
+            });
+        }, plugin, false);
+
         return inventory;
     }
 
@@ -104,6 +183,25 @@ public class CustomInventory {
     public static class ItemStackRegistry {
         int slot;
         ItemStack item;
-        Consumer<ItemStackAction> action;
+        ItemStackAction action;
+    }
+
+
+    @Setter
+    @Getter
+    @AllArgsConstructor
+    @Accessors(fluent = true, chain = true)
+    public static class Holder implements InventoryHolder {
+        CustomInventory custom;
+
+        @Override
+        public @NonNull Inventory getInventory() {
+            return custom.inventory;
+        }
+    }
+
+    @FunctionalInterface
+    public interface Update {
+        List<ItemStackRegistry> update(CustomInventory custom, Inventory inventory, List<ItemStackRegistry> registries);
     }
 }
