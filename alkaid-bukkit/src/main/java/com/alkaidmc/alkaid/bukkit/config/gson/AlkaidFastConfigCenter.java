1c962ad433f23770a6665aa41fcb773861868fd1
钟小白Core <42798150+Core2002@users.noreply.github.com>
2024-05-18 20:02:23 +0800

    ✨ Feat: Add AlkaidFastConfigCenter.
    * ✨ Feat: Add AlkaidFastConfigCenter.
    
    * 👷 Chore: Apply the Apache 2.0 open source license.

---
+++ alkaid-bukkit/src/main/java/com/alkaidmc/alkaid/bukkit/config/gson/AlkaidFastConfigCenter.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2022 Alkaid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *           http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alkaidmc.alkaid.bukkit.config.gson;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.experimental.Accessors;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AlkaidFastConfigCenter {
+    @Getter
+    @Accessors(fluent = true)
+    @SuppressWarnings("unused")
+    @RequiredArgsConstructor(staticName = "of")
+    public static class Pair<F, S> {
+        final F first;
+        final S second;
+    }
+
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private final Map<String, Pair<File, JsonObject>> jsonFiles = new HashMap<>();
+
+    private JsonObject getConfigObject(String fileName) {
+        if (jsonFiles.containsKey(fileName)) {
+            return jsonFiles.get(fileName).second();
+        } else {
+            File configFile = getConfigFile(fileName);
+            JsonObject configObject;
+            if (configFile.isFile()) {
+                String jsonText = readFile(configFile);
+                configObject = gson.fromJson(jsonText, JsonObject.class);
+            } else {
+                configObject = new JsonObject();
+            }
+            jsonFiles.put(fileName, Pair.of(configFile, configObject));
+            return configObject;
+        }
+    }
+
+    public void makeDefaultConfig(String fileName, Object def) {
+        JsonObject defaultObject = gson.fromJson(gson.toJson(def), JsonObject.class);
+        JsonObject configObject = getConfigObject(fileName);
+
+        defaultObject.keySet().forEach(it -> {
+            if (!configObject.has(it)) {
+                configObject.add(it, defaultObject.get(it));
+            }
+        });
+        save(fileName);
+    }
+
+    public <T> T readSnapshot(String fileName, Class<T> type) {
+        return gson.fromJson(gson.toJson(getConfigObject(fileName)), type);
+    }
+
+    private File getConfigFile(String fileName) {
+        return new File(fileName);
+    }
+
+    public JsonElement getValue(String fileName, String key) {
+        var element = getConfigObject(fileName).get(key);
+        if (element == null) {
+            element = new JsonObject();
+        }
+        return element;
+    }
+
+    public void setValue(String fileName, String key, JsonElement value) {
+        getConfigObject(fileName).add(key, value);
+        save(fileName);
+    }
+
+    public void saveAll() {
+        jsonFiles.keySet().forEach(this::save);
+    }
+
+    private void save(String fileName) {
+        if (jsonFiles.containsKey(fileName)) {
+            File file = jsonFiles.get(fileName).first();
+            JsonObject obj = jsonFiles.get(fileName).second();
+            file.getParentFile().mkdirs();
+            saveFile(file, gson.toJson(obj));
+            System.out.println(obj);
+        } else {
+            System.out.println("保存失败：$fileName");
+        }
+    }
+
+    @SneakyThrows
+    private void saveFile(File file, String text) {
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        fileOutputStream.write(text.getBytes(StandardCharsets.UTF_8));
+        fileOutputStream.close();
+    }
+
+    @SneakyThrows
+    private String readFile(File file) {
+        FileInputStream fileInputStream = new FileInputStream(file);
+        String text = new String(fileInputStream.readAllBytes(), StandardCharsets.UTF_8);
+        fileInputStream.close();
+        return text;
+    }
+}
